ee75f2a776de48110e02752beaf0b75c677b35f9
Aaron Calander <aaroncalander@yahoo.com>
2017-04-08 15:35:37 -0700

    Rewrites rotes file

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,11 @@
 class WelcomeController < ApplicationController
 
   def index
+    # Welcome to Mission Trail
   end
+
+  def show
+    # Game description
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :games
 
-  root to: 'games#index'
+  root to: 'welcome#index'
+
 end
