dc0a844339d1b69629a42dc144447affa96afa0c
Jesse Calton <jessecalton@gmail.com>
2017-04-08 12:30:23 -0700

    made game controller and initializes game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,16 +12,35 @@ class GamesController < ApplicationController
     if params[:game][:occupation] == "Yoga Instructor"
       @game = Game.new(
         username: params[:game][:username],
+        fomo: 30,
+        battery: 20,
+        money: 35,
+        occupation: params[:game][:occupation],
+        )
+    elsif params[:game][:occupation] == "Hedge Fund Manager"
+      @game = Game.new(
+        username: params[:game][:username],
         fomo: 50,
         battery: 20,
-        money: 20,
+        money: 70,
+        occupation: params[:game][:occupation],
+        )
+    elsif params[:game][:occupation] == "Tech Bro"
+      @game = Game.new(
+        username: params[:game][:username],
+        fomo: 50,
+        battery: 40,
+        money: 50,
         occupation: params[:game][:occupation],
         )
     end
-    p @game
+    redirect_to @game
     
 
   end
 
+  def show
+    @game = Game.find(params[:id])
+  end
 
 end
