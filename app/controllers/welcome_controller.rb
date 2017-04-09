235b96442f8b455d600c9e49c64a99bbe709310c
Aaron Calander <aaroncalander@yahoo.com>
2017-04-08 17:34:52 -0700

    Adds welcome_controller with button to start game

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,13 @@ class WelcomeController < ApplicationController
 
   def index
     gon.gametext = "Mission Trail"
+    gon.gameimage = "assets/couple.jpg"
+  end
+
+  def fomo_details
+  end
+
+  def game_details
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   root to: 'welcome#index'
 
+  get '/welcome/fomo_details', to: 'welcome#fomo_details', as: 'fomo_details'
 end
