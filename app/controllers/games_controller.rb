0bf6006c375ceb6a21fc405bc32610b19e957fb6
Christopher Huynh <email": null>
2017-04-09 12:05:13 -0700

    testing game image

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,7 +8,7 @@ class GamesController < ApplicationController
     gon.battery = "140"
     gon.time = "120"
     gon.money = "$40"
-    gon.gameimage = "assets/instagram.png"
+    gon.gameimage = "assets/googlemaps.png"
   end
 
   def new
