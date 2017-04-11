eecd95847d1419b3621b2a96bc0d7e4a1bda7670
“Vilde <vilde@vevatne.no>
2017-04-11 14:49:11 -0700

    Adds link to new game

--- app/controllers/game_end_controller.rb
+++ app/controllers/game_end_controller.rb
@@ -3,6 +3,7 @@ class GameEndController < ApplicationController
   def index
     @game = Game.find(session[:id])
     @fomo = @game.fomo
+    @new = false 
 
       if @fomo > 80
         @event = Event.find_by(name: "bad_game_ending")
